37d54b3f69f8d9301edd73666eeefd64cd2fb1aa
amrrs <amrrs@outlook.com>
2016-05-27 18:56:59 +0530

    Update channelvideos_extract.py
    added csv writing functions

--- channelvideos_extract.py
+++ channelvideos_extract.py
@@ -1,9 +1,10 @@
 import time
-#import pandas as pd
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+import csv
 
 browser = webdriver.Chrome()
 
@@ -17,7 +18,7 @@ elem = browser.find_element_by_tag_name("body")
 
 #considering the number of videos in your channel change this below figure
 
-no_of_loadmore = 20
+no_of_loadmore = 100
 
 try:
     while no_of_loadmore:
@@ -30,10 +31,22 @@ except:
 #scrape the content by xpath
 
 post_elems = browser.find_elements_by_xpath('//div/h3/a')
+links = browser.find_elements_by_xpath('//*[@id="channels-browse-content-grid"]/li/div/div[1]/div[2]/h3/a')
+
+with open('googleindia_vids.csv', 'w',newline='') as csvfile:
+    autowriter = csv.writer(csvfile)
+    autowriter.writerow(['Title','URL'])
+    for i in range(0,len(post_elems)-1):
+        autowriter.writerow([post_elems[i].text,links[i].get_attribute('href')])
 
 #iterate thru the elements and print it
 
 for post in post_elems:
-    print(post.text)
+  print(post.text) 
+
+for link in links:
+    print(link.get_attribute('href'))
+
+
 
 
